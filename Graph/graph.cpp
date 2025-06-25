6cfb16e70577da359bbf78ee23599c7d7665a0a4
yash7xm <yash.7xm@gmail.com>
2025-06-25 14:59:57 +0530

    graphs added

---
+++ Graph/graph.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define all(v) (v).begin(), (v).end()
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+
+void solve() {
+    // your code here
+    int n, m;
+    cin >> n >> m;
+    cout << n << " " << m;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t; // uncomment for multiple test cases
+    while (t--) solve();
+    return 0;
+}
+    
